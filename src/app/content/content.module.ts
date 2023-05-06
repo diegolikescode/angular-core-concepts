b0a837921cbbb75d6828f401475749b93fe4d023
diegolikescode <diegoprestes.git@hotmail.com>
2023-05-06 17:02:51 -0300

    prepare buttons props

--- src/app/content/content.module.ts
+++ src/app/content/content.module.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home/home.component';
 import { MyReposComponent } from './my-repos/my-repos.component';
 import { GuardSigninComponent } from './guard-signin/guard-signin.component';
+import { LoginComponent } from './login/login.component';
 
 
 
 @NgModule({
-  declarations: [HomeComponent, MyReposComponent, GuardSigninComponent],
+  declarations: [HomeComponent, MyReposComponent, GuardSigninComponent, LoginComponent],
   imports: [
     CommonModule
   ]
--- src/app/header/full-header/full-header.component.ts
+++ src/app/header/full-header/full-header.component.ts
@@ -9,16 +9,30 @@ export class FullHeaderComponent {
   @Input() componentToShow:
     | "home"
     | "get-repos"
-    | "need-signin"
-    | "signin"
+    | "guard"
+    | "login"
     | "signout" = "home";
 
-  @Output() clickedButton: string; // exportado pelo header ou pelo pai de todos?
-  @Output() onClickEvent = function () {
-    // this.componentToShow
-  };
+  @Output() userSignedIn = false;
+  @Output() contentSelected: string; // exportado pelo header ou pelo pai de todos? => event emiter!
 
   @Output() onClickHome = function () {
     this.componentToShow = "home";
+    console.log(this.componentToShow);
+  };
+
+  @Output() onClickMyRepos = function () {
+    this.componentToShow = "get-repos";
+    console.log(this.componentToShow);
+  };
+
+  @Output() onClickGuard = function () {
+    this.componentToShow = "guard";
+    console.log(this.componentToShow);
+  };
+
+  @Output() onClickLogin = function () {
+    this.componentToShow = "login";
+    console.log(this.componentToShow);
   };
 }
