cd5009725a3e60e27ec26bb9bde0f88010cd194b
diegolikescode <diegoprestes.git@hotmail.com>
2023-05-07 17:38:41 -0300

    feat: init routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
+import { HomeComponent } from "./content/home/home.component";
+import { MyReposComponent } from "./content/my-repos/my-repos.component";
+import { GuardSigninComponent } from "./content/guard-signin/guard-signin.component";
+import { LoginComponent } from "./content/login/login.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  // { path: "", component: HomeComponent },
+  { path: "my-repos", component: MyReposComponent },
+  {
+    path: "guard",
+    component: GuardSigninComponent,
+  },
+  { path: "login", component: LoginComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
